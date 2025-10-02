c060ae0924e2e7363bfa1a7ca64dce13639ef2aa
ains2054 <masadlawan@nemsu.edu.ph>
2025-10-02 11:51:08 +0800

    firebase authentication na ako

--- lib/controllers/auth_controller.dart
+++ lib/controllers/auth_controller.dart
@@ -6,35 +6,43 @@ class AuthController {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
   Future<String> registerNewUser(
-    String email, String name, String password
+    String email,
+    String name,
+    String password,
   ) async {
     String result = 'something went Wrong';
     try {
       UserCredential userCredential = await _auth
-        .createUserWithEmailAndPassword(email: email, password: password);
+          .createUserWithEmailAndPassword(email: email, password: password);
 
-        await _firestore.collection('singers').doc(userCredential.user!.uid).set({
-          'name': name,
-          'profileImage': "",
-          'uid': userCredential.user! .uid,
-          'pinCode': "",
-          'purok': '',
-          'city': '',
-        });
+      await _firestore.collection('singers').doc(userCredential.user!.uid).set({
+        'name': name,
+        'profileImage': "",
+        'uid': userCredential.user!.uid,
+        'pinCode': "",
+        'purok': '',
+        'city': '',
+      });
 
-        result = 'success';
-    } catch (e){
+      result = 'success';
+    } catch (e) {
       result = e.toString();
     }
     return result;
   }
 
-  Future <String> loginUser(String email, String password) async{
-    String result = 'something went wrong' ;
+  Future<String> loginUser(String email, String password) async {
+    String result = 'something went wrong';
     try {
       await _auth.signInWithEmailAndPassword(email: email, password: password);
       result = "success";
-    } catch (e){
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        print('The password provided is too weak.');
+      } else if (e.code == 'email-already-in-use') {
+        print('The account already exists for that email.');
+      }
+    } catch (e) {
       result = e.toString();
     }
     return result;
